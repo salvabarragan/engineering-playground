f1dd02f1dec7a7e53ef8cec25585b3c88504c3a0
Salva <salvabarragantorres@gmail.com>
2026-07-30 13:40:34 +0200

    feat: add name validation to Task class and add unit tests

--- src/task.py
+++ src/task.py
@@ -3,15 +3,19 @@ import datetime as dt
 
 class Task:
     def __init__(self, name: str, date: str, category: str):
-        self.name = name
+        if name != "": # Name cannot be empty
+            self.name = name
+        else:
+            raise ValueError("'Name' cannot be empty")
+            
         try:
             self.date = dt.datetime.strptime(date, "%d/%m/%Y")
         except ValueError:
             raise
 
-        self.category = category
+        self.category = category # Tasks do not necessary need a category, is optional
         self.is_done = False
-    
+
     def complete_task(self):
         self.is_done = True
 
---
+++ tests/test_task.py
@@ -0,0 +1,31 @@
+import pytest
+from datetime import datetime
+from src.task import Task
+
+
+@pytest.fixture
+def task():
+    return Task(name="Study maths", date="01/01/2027", category="School")
+
+
+class TestTask:
+    def test_task_creation(self, task):
+        assert task.name == "Study maths"
+        assert task.date == datetime.strptime("01/01/2027", "%d/%m/%Y")
+        assert task.category == "School"
+        assert task.is_done is False
+
+    def test_invalid_date_task_creation(self):
+        with pytest.raises(ValueError):
+            Task(name="Study maths", date="32/01/2027", category="School")
+            
+    def test_invalid_name_task_creation(self):
+        with pytest.raises(ValueError):
+            Task(name="", date="01/01/2027", category="School")
+
+    def test_complete_task(self, task):
+        task.complete_task()
+        assert task.is_done is True
+
+    def test_show_task(self, task):
+        assert str(task) == "Study maths (School) - 01/01/2027 (Undone)"
